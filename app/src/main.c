efb69fe4515916ead3ff4babc6d4e93a403fb8e3
bastien <bastien.auneau@tsat.net>
2026-02-24 12:23:21 +0100

    Set/Get freq and resolution

--- app/src/main.c
+++ app/src/main.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/sensor.h>
 //#include <zephyr/drivers/gpio.h>
 
 #include <zephyr/logging/log.h>
@@ -24,21 +25,72 @@ int main(void)
 
 	const struct device *dev = DEVICE_DT_GET_ONE(st_vl53l8cx);
 
-    LOG_INF("is dev ready");
     if (!device_is_ready(dev)) {
         LOG_INF("dev NOT ready");
         return 0;
     }
-	LOG_INF("dev ready");
+	LOG_INF("%s ready", dev->name);
 
-    //struct sensor_value val;
-    //sensor_sample_fetch(dev);
-    //sensor_channel_get(dev, SENSOR_CHAN_DISTANCE, &val);
+    ////////////////////////////////////////////////////////
+    struct sensor_value sample_freq = { .val1 = 10, .val2 = 0 };
 
-	LOG_INF("STOP  ----");
+    printk("Setting frequency to 10 Hz...\n");
+    ret = sensor_attr_set(
+        dev, 
+        SENSOR_CHAN_DISTANCE, 
+        SENSOR_ATTR_SAMPLING_FREQUENCY, 
+        &sample_freq
+    );
+    if (ret != 0) {
+        LOG_ERR("Failed to set sample frequency (error %d)\n", ret);
+    }
+
+    ret = sensor_attr_get(
+        dev, 
+        SENSOR_CHAN_DISTANCE, 
+        SENSOR_ATTR_SAMPLING_FREQUENCY, 
+        &sample_freq
+    );
+    if (ret != 0) {
+        LOG_ERR("Failed to get sample frequency (error %d)\n", ret);
+    }
+    LOG_INF("Read back sample freq: %d", sample_freq.val1);
+
+    struct sensor_value resolution = { .val1 = 16, .val2 = 0 };
+    sensor_attr_set(
+        dev, 
+        SENSOR_CHAN_DISTANCE, 
+        SENSOR_ATTR_RESOLUTION, 
+        &resolution
+    );
+    ///////////////////////////////////////////////////////////////
+#if 0
+    /* 2. PREPARE: Get Standard Decoder */
+    const struct sensor_decoder_api *decoder;
+    sensor_get_decoder(dev, &decoder);
+
+    uint8_t raw_buffer[128]; // Allocate stack buffer for 8x8
+
+    while (1) {
+        /* 3. EXECUTE: Call your CUSTOM driver helper */
+        /* This keeps I2C logic inside the driver, but runs synchronously */
+        my_lidar_manual_read(dev, raw_buffer, sizeof(raw_buffer));
+
+        /* 4. DECODE: Use Standard Decoder */
+        struct sensor_value point_cloud[64];
+        uint32_t fit = 0;
+        
+        decoder->decode(raw_buffer, SENSOR_CHAN_DISTANCE, 
+                        &fit, 64, point_cloud);
+
+        printk("Zone 0: %d\n", point_cloud[0].val1);
+        k_msleep(100);
+    }
+#endif
+    ///////////////////////////////////////////////////////////////
 
     while (true) {
-        k_msleep(3000);
+        k_msleep(10000);
         LOG_INF("mmmmmmmmm ----");
     }
 	return 0;
