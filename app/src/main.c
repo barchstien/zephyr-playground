4bb5eb4267791dc9f1f1ada207145dc981404b2b
bastien <bastien.auneau@tsat.net>
2026-03-13 00:08:59 +0100

    RTIO stream, work like a charm, kept one shot read behind define. Grid only show color, not text anymore. Emit on UDP struct data, but doesn't use q15 yet

--- app/src/main.c
+++ app/src/main.c
@@ -12,11 +12,13 @@
 #include <zephyr/net/socket.h>
 #include <zephyr/rtio/rtio.h>
 
+#include <zephyr/drivers/sensor/vl53l8cx.h>
 
 LOG_MODULE_REGISTER(playground_app, LOG_LEVEL_INF);
+
 // debug
 //#include <zephyr/sys/printk.h>
-#include "vl53l8cx_debug.h"
+//#include "vl53l8cx_debug.h"
 
 #define DEFAULT_SAMPLE_FREQ_HZ 15
 #define DEFAULT_ZONE_CNT 64
@@ -31,19 +33,78 @@ LOG_MODULE_REGISTER(playground_app, LOG_LEVEL_INF);
  * Args: name, sq_size (pow 2), cq_size (pow 2), mem_blocks, block_byte_size, alignment 
  */
 RTIO_DEFINE_WITH_MEMPOOL(
-    tof_rtio_ctx,           // name
-    2,//8,                      // sq size
-    2,//8,                      // cq size
-    4,//16,                     // num of blocks
-    sizeof(VL53L8CX_ResultsData) + 5,// 1 byte (num of zone) + 4 bytes timepoint msec + 2 bytes per zone
-    sizeof(void *)          // byte aligne
+    vl53l8cx_rtio_ctx,           // name
+    8,                      // submission
+    8,                      // completion
+    16,                     // num of blocks
+    VL53L8CX_SENSOR_READ_BLOCK_SIZE,
+    sizeof(void *)          // byte aligned
 );
 
-SENSOR_DT_READ_IODEV(
-    tof_iodev, 
+#define VL53L8CX_STREAM
+#ifdef VL53L8CX_STREAM
+SENSOR_DT_STREAM_IODEV(
+    vl53l8cx_iodev, 
     DT_INST(0, st_vl53l8cx), 
     { SENSOR_CHAN_DISTANCE, 0 }
 );
+#else
+SENSOR_DT_READ_IODEV(
+    vl53l8cx_iodev, 
+    DT_INST(0, st_vl53l8cx), 
+    { SENSOR_CHAN_DISTANCE, 0 }
+);
+#endif
+
+void wait_and_read(const struct sensor_decoder_api *decoder, int sock,
+                    struct sockaddr_in *dest_addr) {
+    struct rtio_cqe *cqe;
+    uint8_t *sensor_data_buff;
+	uint32_t sensor_data_buff_len;
+    struct sensor_chan_spec ch_spec = { SENSOR_CHAN_DISTANCE, 0 };
+    static struct vl53l8cx_result_data result;
+    int ret;
+
+    // wait for a completion event
+    cqe = rtio_cqe_consume_block(&vl53l8cx_rtio_ctx);
+    if (cqe->result < 0) {
+        LOG_ERR("cqe failed with result: %d", cqe->result);
+        rtio_cqe_release(&vl53l8cx_rtio_ctx, cqe);
+        return;
+    }
+
+    // Get data from completion queue element
+    ret = rtio_cqe_get_mempool_buffer(&vl53l8cx_rtio_ctx, cqe, &sensor_data_buff, &sensor_data_buff_len);
+    if (ret == 0) {
+        // decode the data, always comes 1 by 1
+        uint32_t fit = 0;
+        ret = decoder->decode(sensor_data_buff, ch_spec, &fit, 1, &result);
+        rtio_release_buffer(&vl53l8cx_rtio_ctx, sensor_data_buff, sensor_data_buff_len);
+        
+        // inverse image
+        q15_t tmp[64];
+        memcpy(tmp, result.readings[0].distance_mm, sizeof(q15_t[64]));
+        for (int i=0; i<8; i++) {
+            for (int j=0; j<8; j++) {
+                result.readings[0].distance_mm[8*i + j] = tmp[8*(i+1) - (j + 1)];
+            }
+        }
+
+        // send to UDP
+        ret = zsock_sendto(
+            sock, 
+            (uint8_t*)(&result), 
+            sizeof(struct vl53l8cx_result_data), 
+            0, 
+            (struct sockaddr*)dest_addr, 
+            sizeof(struct sockaddr_in)
+        );
+        if (ret < 0) {
+            LOG_ERR("UDP sendto failed errno:%d", errno);
+        }
+    }
+    rtio_cqe_release(&vl53l8cx_rtio_ctx, cqe);
+}
 
 int main(void)
 {
@@ -87,7 +148,7 @@ int main(void)
     }
     LOG_INF("Read back resolution: %d", resolution.val1);
 
-    // sample freq, starts sampling if freq > 0
+    // sample freq
     struct sensor_value sample_freq = { .val1 = DEFAULT_SAMPLE_FREQ_HZ, .val2 = 0 };
     LOG_INF("Setting frequency to %d Hz...", sample_freq.val1);
     ret = sensor_attr_set(
@@ -117,153 +178,39 @@ int main(void)
     // UDP
     int sock;
     struct sockaddr_in dest_addr;
-    // 1. Create a UDP socket (AF_INET = IPv4, SOCK_DGRAM = UDP)
     sock = zsock_socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     if (sock < 0) {
         LOG_ERR("Failed to create socket: %d", errno);
-        return -51;
+        return -EIO;
     }
-    // 2. Setup destination address
+    // destination address
     dest_addr.sin_family = AF_INET;
     dest_addr.sin_port = htons(DEST_PORT);
     zsock_inet_pton(AF_INET, DEST_ADDR, &dest_addr.sin_addr);
-    
 
-#if 1
-    // trigger
-    //struct sensor_trigger trig = {
-    //    .type = SENSOR_TRIG_DATA_READY,
-    //    .chan = SENSOR_CHAN_DISTANCE,
-    //};
-    ///* Register the ISR */
-    //if (sensor_trigger_set(dev, &trig, vl53l8cx_rdy_callback) < 0) {
-    //    return -EIO;
-    //}
-    ///////////////////////////////////////////////////////////////
-    ///////////////////////////////////////////////////////////////
-    struct rtio_cqe *cqe;
-    uint8_t *buf;
-	uint32_t buf_len;
-    struct sensor_chan_spec ch_spec = { SENSOR_CHAN_DISTANCE, 0 };
-    uint8_t data_out[1+4+64*2]; // TODO 
-    uint8_t data_tmp[1+4+64*2];
-    int i = 0;
-
-    for (i=0; i<1000000; i++) {
-        /* Queue async read to the RTIO context; uses RTIO mempool for allocation */
-		ret = sensor_read_async_mempool(&tof_iodev, &tof_rtio_ctx, NULL);
+    //// main loop
+#ifdef VL53L8CX_STREAM
+    //// streaming
+    struct rtio_sqe *sqe;
+    // sensor_stream
+    LOG_INF("sensor_stream()...");
+    sensor_stream(&vl53l8cx_iodev, &vl53l8cx_rtio_ctx, NULL, &sqe);
+    while (true) {
+        wait_and_read(decoder, sock, &dest_addr);
+    }
+#else
+    //// one-shot in a loop
+    while (true) {
+		ret = sensor_read_async_mempool(&vl53l8cx_iodev, &vl53l8cx_rtio_ctx, NULL);
 		if (ret < 0) {
 			LOG_ERR("Failed to submit read: %d", ret);
 			continue;
 		}
-
-#if 0
-        /* Block and consume the completion event */
-		cqe = rtio_cqe_consume_block(&tof_rtio_ctx);
-#endif
-        //cqe = rtio_cqe_consume(&tof_rtio_ctx);
-        //while (cqe == 0) {
-        //    //LOG_INF(" ... ");
-        //    k_sleep(K_MSEC(1));
-        //    cqe = rtio_cqe_consume(&tof_rtio_ctx);
-        //}
-        cqe = rtio_cqe_consume_block(&tof_rtio_ctx);
-        // what about using this:
-        // Wait up to 500ms for exactly 1 completion event
-        //int copied = rtio_cqe_copy_out(&tof_rtio_ctx, &cqe, 1, K_MSEC(500));
-
-        
-        //LOG_INF("Got CQE !");
-
-		if (cqe->result < 0) {
-			LOG_ERR(" *** Read failed with result: %d", cqe->result);
-			rtio_cqe_release(&tof_rtio_ctx, cqe);
-			continue;
-		}
-
-		/* Pull buffer pointer generated by the driver's rx_buf pool execution */
-		ret = rtio_cqe_get_mempool_buffer(&tof_rtio_ctx, cqe, &buf, &buf_len);
-		if (ret == 0) {
-			uint32_t fit = 0;
-			ret = decoder->decode(buf, ch_spec, &fit, 1, &data_tmp);
-            rtio_release_buffer(&tof_rtio_ctx, buf, buf_len);
-            // header
-            memcpy(data_out, data_tmp, 5);
-            // inverse image
-#if 1
-            uint16_t *src = (int16_t*)(&data_tmp[5]);
-            uint16_t *dst = (int16_t*)(&data_out[5]);
-            for (int i=0; i<8; i++) {
-                for (int j=0; j<8; j++) {
-                    dst[8*i + j] = src[8*(i+1) - (j + 1)];
-                    //dst[i*8+j] = src[i*8+j];
-                }
-            }
-#else
-            memcpy(data_out + 5, data_tmp + 5, 64*2);
-#endif
-
-#if 0
-            /////
-            // Use [0 : 99] cm
-            //printk("blop %d", ret);
-            uint8_t pretty[200];
-            memset(pretty, 0, sizeof(pretty));
-            uint8_t ind = 0;
-            for (int i=0; i<64; i++) {
-                // mm -> cm
-                data_out[i] =  data_out[i] / 10;
-                if (data_out[i] >= 100) { data_out[i] = 99; }
-                if (i % 8 == 0) {
-                    //printk("\n");
-                    snprintf(pretty + ind, sizeof(pretty) - ind, "\n");
-                    ind += 1;
-                }
-                //printk("%d%d ", data_out[i]/10, data_out[i]%10);
-                if (data_out[i] == 99)
-                    snprintf(pretty + ind, sizeof(pretty) - ind, "   ");
-                else
-                    snprintf(pretty + ind, sizeof(pretty) - ind, "%d%d ", data_out[i]/10, data_out[i]%10);
-                ind += 3;
-            }
-            //printk("\n");
-            snprintf(pretty + ind, sizeof(pretty) - ind, "\n");
-            ind += 1;
-            //LOG_INF("%s", pretty);
-            printf("%s\n", pretty);
-#endif
-            int data_out_size = 1 + 4 + 64*2; // TODO
-            zsock_sendto(
-                sock, data_out, data_out_size, 0, 
-                (struct sockaddr *)&dest_addr, sizeof(dest_addr)
-            );
-
-			
-		}
-
-		rtio_cqe_release(&tof_rtio_ctx, cqe);
-        //LOG_INF("------");
-		//k_sleep(K_MSEC(10));
-        //k_sleep(K_MSEC(250));
+        wait_and_read(decoder, sock, &dest_addr);
+        // TODO go to sleep, wake up, etc)
+        k_msleep(1000);
     }
-    // stop sampling
-    sample_freq.val1 = 0;
-    ret = sensor_attr_set(
-        dev, 
-        SENSOR_CHAN_DISTANCE, 
-        SENSOR_ATTR_SAMPLING_FREQUENCY, 
-        &sample_freq
-    );
 #endif
-
     zsock_close(sock);
 
-#if 0
-    while (true) {
-        //k_msleep(10000);
-        //LOG_INF("mmmmmmmmm ----");
-        test_read(dev, buff, BUFF_DEBUG_SIZE);
-    }
-	return 0;
-#endif
 }
--- visualisation/app.py
+++ visualisation/app.py
@@ -2,6 +2,7 @@ import socket
 import struct
 import pygame
 import sys
+import colorsys
 
 # --- CONFIGURATION ---
 UDP_IP = "0.0.0.0"
@@ -24,114 +25,8 @@ BG_COLOR = (30, 30, 34)
 TEXT_COLOR = (220, 220, 220)
 EMPTY_CELL_COLOR = (50, 50, 55)
 
+
 def get_cell_color(dist_mm):
-    """ Map distance (0-4000) to a Red -> Yellow -> Green colormap. """
-    dist = max(0, min(dist_mm, MAX_DISTANCE))
-    ratio = dist / float(MAX_DISTANCE)
-    
-    if ratio < 0.5:
-        # Red to Yellow
-        r = 255
-        g = int((ratio * 2) * 255)
-        b = 0
-    else:
-        # Yellow to Green
-        r = int((1.0 - (ratio - 0.5) * 2) * 255)
-        g = 255
-        b = 0
-        
-    return (r, g, b)
-
-def get_cell_color2(dist_mm):
-    """ 
-    High-granularity multi-stop color mapping.
-    Maps 0-4000mm to a smooth Purple -> Blue -> Green -> Yellow -> Red gradient.
-    """
-    # 1. Clamp distance and normalize to 0.0 - 1.0
-    val = max(0, min(dist_mm, MAX_DISTANCE))
-    ratio = val / float(MAX_DISTANCE)
-
-    # 2. Define color stops (R, G, B)
-    # You can tweak these to change the 'feel' of the heatmap
-    colors = [
-        (255, 255, 255),   # 0%   - GESTURE PEAK (Pure White Glow)
-        (0, 255, 255),     # 25%  - ACTIVE ZONE (Bright Cyan)
-        (0, 100, 255),     # 50%  - VISIBLE (Azure Blue)
-        (70, 0, 150),      # 75%  - BACKGROUND (Deep Purple)
-        (20, 20, 30)       # 100% - SLEEP (Dark Navy Grey)
-    ]
-
-    # 3. Determine which segment of the gradient we are in
-    segment_float = ratio * (len(colors) - 1)
-    index = int(segment_float)
-    inner_ratio = segment_float - index # How far between index and index + 1
-
-    if index >= len(colors) - 1:
-        return colors[-1]
-
-    # 4. Linear Interpolation (Lerp) between the two colors
-    c1 = colors[index]
-    c2 = colors[index + 1]
-
-    r = int(c1[0] + (c2[0] - c1[0]) * inner_ratio)
-    g = int(c1[1] + (c2[1] - c1[1]) * inner_ratio)
-    b = int(c1[2] + (c2[2] - c1[2]) * inner_ratio)
-
-    return (r, g, b)
-
-def get_cell_color3(dist_mm):
-    """
-    High-Granularity Gesture Colormap.
-    Focuses the entire color resolution on the 0-1200mm range.
-    """
-    # 1. SETUP THE ACTIVE WINDOW
-    # Gestures happen close to the sensor. By ignoring the 'far' background,
-    # we 'zoom in' on the values that matter.
-    MIN_GESTURE_DIST = 50   # mm
-    MAX_GESTURE_DIST = 600 # mm 
-
-    if dist_mm == 0 or dist_mm > MAX_DISTANCE: return (20, 20, 25) # Dark Background
-    if dist_mm > MAX_GESTURE_DIST: return (40, 40, 60)      # Muted Far Plane
-
-    # 2. NORMALIZE WITHIN THE WINDOW
-    # This makes 50mm a much larger percentage of the total scale.
-    val = max(MIN_GESTURE_DIST, min(dist_mm, MAX_GESTURE_DIST))
-    ratio = (val - MIN_GESTURE_DIST) / float(MAX_GESTURE_DIST - MIN_GESTURE_DIST)
-
-    # 3. TURBO-INSPIRED HIGH-DENSITY GRADIENT (11 STOPS)
-    # Designed to maximize 'local' contrast so 50mm changes are visible.
-    # Order: Near (0.0) -> Far (1.0)
-    colors = [
-        (255, 255, 255), # 0.00 - Touch (White)
-        (255, 0, 255),   # 0.10 - Magenta
-        (180, 0, 255),   # 0.20 - Deep Purple
-        (0, 0, 255),     # 0.30 - Blue
-        (0, 150, 255),   # 0.40 - Sky Blue
-        (0, 255, 255),   # 0.50 - Cyan
-        (0, 255, 150),   # 0.60 - Teal
-        (0, 255, 0),     # 0.70 - Green
-        (150, 255, 0),   # 0.80 - Lime
-        (255, 255, 0),   # 0.90 - Yellow
-        (255, 150, 0)    # 1.00 - Orange
-    ]
-
-    # 4. Interpolate
-    segment_float = ratio * (len(colors) - 1)
-    idx = int(segment_float)
-    inner_ratio = segment_float - idx
-
-    if idx >= len(colors) - 1: return colors[-1]
-
-    c1, c2 = colors[idx], colors[idx + 1]
-    return (
-        int(c1[0] + (c2[0] - c1[0]) * inner_ratio),
-        int(c1[1] + (c2[1] - c1[1]) * inner_ratio),
-        int(c1[2] + (c2[2] - c1[2]) * inner_ratio)
-    )
-
-import colorsys # Make sure to add this at the top of your file!
-
-def get_cell_color4(dist_mm):
     """
     Maximum Contrast Depth Map for 50-600mm range.
     Uses continuous HSV (Hue, Saturation, Value) math instead of stops.
@@ -178,7 +73,8 @@ def get_text_color_for_bg(bg_color):
 def main():
     # Initialize Pygame
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    flags = pygame.DOUBLEBUF
+    screen = pygame.display.set_mode((WIDTH, HEIGHT), flags, vsync=1)
     pygame.display.set_caption("ToF 8x8 Lidar Viewer")
     clock = pygame.time.Clock()
 
@@ -191,11 +87,12 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
     sock.setblocking(False)  # Non-blocking so our UI loop never freezes
+    #sock.settimeout(0.001)
 
     # State variables
-    last_timestamp = 0
-    last_zone_cnt = 0
-    last_matrix = [0] * 64
+    timestamp_ns = 0
+    resolution = 0
+    d_matrix = [0] * 64
     has_data = False
 
     running = True
@@ -211,23 +108,29 @@ def main():
         packet_data = None
         try:
             while True:
-                data, addr = sock.recvfrom(1024)
-                if len(data) == EXPECTED_SIZE:
-                    packet_data = data
-                    #print(data.hex(' '), '\n')
-                else:
-                    print('wrong length: ', len(data))
+                packet_data, addr = sock.recvfrom(1024)
         except BlockingIOError:
             pass # No more data in the socket buffer
+        #except TimeoutError:
+        #    pass
 
         # 3. Parse Data
-        if packet_data:
-            # Format: < (Little Endian), B (1 Byte), Q (8 Bytes), 64H (64 x 2-byte unsigned shorts)
-            unpacked = struct.unpack('<B I 64H', packet_data)
-            last_zone_cnt = unpacked[0]
-            last_timestamp = unpacked[1]
-            last_matrix = unpacked[2:]
-            has_data = True
+        try:
+            if packet_data:
+                # Format: < (Little Endian), B (1 byte), I (32 bytes), Q (8 bytes), 64H (64 x 2-byte unsigned shorts)
+                unpacked = struct.unpack('<Q H H I B B I 64H', packet_data)
+                timestamp_ns = unpacked[0]
+                reading_count = unpacked[1]
+                # total 6 bytes padding
+                padding1 = unpacked[2]
+                padding2 = unpacked[3]
+                shift = unpacked[4]
+                resolution = unpacked[5]
+                timestamp_delta = unpacked[6]
+                d_matrix = unpacked[7:]
+                has_data = True
+        except struct.error as e:
+            print(e, 'but got instead:', len(packet_data))
 
         # 4. Render UI
         screen.fill(BG_COLOR)
@@ -237,9 +140,9 @@ def main():
         for row in range(GRID_ROWS):
             for col in range(GRID_COLS):
                 idx = row * GRID_COLS + col
-                val = last_matrix[idx] if has_data else 0
+                val = d_matrix[idx] if has_data else 0
                 
-                cell_bg = get_cell_color4(val) if has_data else EMPTY_CELL_COLOR
+                cell_bg = get_cell_color(val) if has_data else EMPTY_CELL_COLOR
                 x = MARGIN + col * CELL_SIZE
                 y = grid_start_y + row * CELL_SIZE
                 
@@ -249,17 +152,18 @@ def main():
 
                 # Draw distance text inside cell
                 if has_data:
-                    cell_text_color = get_text_color_for_bg(cell_bg)
-                    txt_surface = font_cell.render(str(val), True, cell_text_color)
-                    txt_rect = txt_surface.get_rect(center=rect.center)
-                    screen.blit(txt_surface, txt_rect)
+                    pass
+                    #cell_text_color = get_text_color_for_bg(cell_bg)
+                    #txt_surface = font_cell.render(str(int(round(val/10.0, 0))), True, cell_text_color)
+                    #txt_rect = txt_surface.get_rect(center=rect.center)
+                    #screen.blit(txt_surface, txt_rect)
 
         # Draw Footer Text
         footer_y = grid_start_y + (GRID_ROWS * CELL_SIZE) + 20
         
         if has_data:
-            len_text = font_large.render(f"last zone count: {last_zone_cnt}", True, TEXT_COLOR)
-            ts_text = font_large.render(f"Timestamp: {last_timestamp*0.001:.1f}", True, TEXT_COLOR)
+            len_text = font_large.render(f"last zone count: {resolution}", True, TEXT_COLOR)
+            ts_text = font_large.render(f"Timestamp: {timestamp_ns/1e9:.1f}", True, TEXT_COLOR)
         else:
             len_text = font_large.render("Waiting for data...", True, (200, 100, 100))
             ts_text = font_large.render(f"Listening on UDP {UDP_PORT}", True, TEXT_COLOR)
@@ -269,7 +173,7 @@ def main():
 
         # Update Display
         pygame.display.flip()
-        clock.tick(60) # Cap at 60 FPS
+        clock.tick(120) # Cap at 60 FPS
 
     sock.close()
     pygame.quit()
